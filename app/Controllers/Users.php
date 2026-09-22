d21847c899cf3c94ba5fcb7c17f55ecf9391a6a4
JereLim-CC <jeremiahabellim@gmail.com>
2026-09-23 00:50:43 +0800

    Refactor Users controller to use UserModel

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -2,37 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Users extends BaseController
 {
     public function index()
     {
-        $users = [
-            [
-                'username' => 'admin',
-                'full_name' => 'John Admin',
-                'role' => 'Administrator'
-            ],
-            [
-                'username' => 'cashier1',
-                'full_name' => 'Sarah Cruz',
-                'role' => 'Cashier'
-            ],
-            [
-                'username' => 'cashier2',
-                'full_name' => 'Michael Santos',
-                'role' => 'Cashier'
-            ],
-            [
-                'username' => 'manager1',
-                'full_name' => 'David Reyes',
-                'role' => 'Manager'
-            ],
-            [
-                'username' => 'staff1',
-                'full_name' => 'Anna Garcia',
-                'role' => 'Staff'
-            ]
-        ];
+        $userModel = new UserModel();
+
+        $users = $userModel->findAll();
 
         return view('users/index', [
             'users' => $users
